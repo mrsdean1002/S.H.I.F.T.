79ecc977fe0b60086212785133689c30bb991e65
Tamika Dean <mrsdean1002@gmail.com>
2018-03-26 12:17:03 -0400

    added gitignore file, added a create quote field under recipes page, name hasnt been changed yet, created a update button as well but doesnt work at the moment

--- src/server.js
+++ src/server.js
@@ -1,15 +1,28 @@
+// src/server.js
+
+const path = require('path');
 const express = require('express');
+const bodyParser = require('body-parser');
 const config = require('./config');
+const router = require('./routes');
+
+// Load mongoose package
+// const mongoose = require('mongoose');
+
+// Connect to MongoDB and create/use database as configured
+//mongoose.connection.openUri(`mongodb://${config.db.username}:${config.db.password}@${config.db.host}/${config.db.dbName}`);
+
+// Import all models
+// require('./models/file.model.js');
+
 const app = express();
+const publicPath = path.resolve(__dirname, '../html');
+app.use(express.static(publicPath));
+app.use(bodyParser.json());
 
-  app.use(function(req, res, next) {
-  res.end("Hello World!");
-});
+app.use('/api', router);
 
-  app.use('/doc', function(req, res, next) {
-  res.end(`Documentation http://expressjs.com/`);
-});
 
-  app.listen(config.port, function() {
+app.listen(config.port, function() {
   console.log(`${config.appName} is listening on port ${config.port}`);
 });
